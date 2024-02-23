6362c3847c6d785f0dd46a68d833b4b842c62997
carlos arnulfo cogua laverde <engineer.carloscogua@gmail.com>
2024-02-22 22:53:07 -0500

    Aprendiendo a usar debug / supervisor de desarrollo lección 2 tutorial

--- tutorialflask.py
+++ tutorialflask.py
@@ -6,14 +6,14 @@ from flask import Flask, request, make_response, redirect
 app = Flask(__name__) # crea una instancia ( Objeto )de la aplicación Flask
 
 
-@app.route("/index") #* Este decorador se usa para asociar una vista con una ruta URL específica.
+@app.route("/") #* Este decorador se usa para asociar una vista con una ruta URL específica.
 def index():    
     return "Hello World"
 
 #* Comprueba si el módulo se está ejecutando como el programa principal.
 if __name__ == "__main__": #! Cuando se ejecuta un archivo Python, el valor de la variable __name__ se establece como "__main__".
     #*Si el módulo se está ejecutando como el programa principal, ejecuta la aplicación Flask.
-    app.run() #!La función app.run() inicia un servidor web local en el puerto 5000.
-    
+    app.run(debug = True, port= 1991) #!La función app.run() inicia un servidor web local en el puerto 5000.
+#! En resumen, el modo debug es una herramienta útil para principiantes que están aprendiendo a programar. Te ayuda a encontrar errores, entender tu código y probar cosas nuevas. Sin embargo, no es necesario para el producto final y puede ser un poco lento e informativo.
 
 
