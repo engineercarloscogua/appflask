6aca1f1cd3112ba04dfc8874a57d84b976ba49a2
carlos arnulfo cogua laverde <engineer.carloscogua@gmail.com>
2024-02-22 23:39:00 -0500

    Aprendiendo sobre decorador route, variables dentro de las rutas y rutas default lección 3 del tutorial

--- tutorialflask.py
+++ tutorialflask.py
@@ -6,11 +6,65 @@ from flask import Flask, request, make_response, redirect
 app = Flask(__name__) # crea una instancia ( Objeto )de la aplicación Flask
 
 
+#! RUTAS 
+
+#
+#1. Variables estáticas:
+
+#Se definen en la ruta misma.
+#Son valores fijos que no cambian.
+#Ejemplo: /usuario/<nombre>
+
+#2. Variables dinámicas:
+
+#Se extraen de la URL cuando un usuario solicita la ruta.
+#Pueden ser de diferentes tipos, como números, cadenas o fechas.
+#Ejemplo: /usuario/<int:id>
+
+#3. Expresiones regulares:
+
+#Permiten definir patrones más complejos para las variables.
+#Se pueden usar para validar la entrada del usuario.
+#Ejemplo: /usuario/<regex("[a-zA-Z0-9]+")>
+
+#4. Convertidores personalizados:
+
+#Permiten crear tus propios tipos de variables.
+#Se pueden usar para manejar diferentes tipos de datos.
+#Ejemplo: Un convertidor para convertir una cadena a una fecha.
+
+#!RUTA SIN VARIABLES NI EXTENCIONES
 @app.route("/") #* Este decorador se usa para asociar una vista con una ruta URL específica.
 def index():    
     return "Hello World"
 
-#* Comprueba si el módulo se está ejecutando como el programa principal.
+#! RUTA CON VARIABLE ENTERA
+@app.route("/valor/<int:n>")
+def valor(n):
+    return "numero: {}".format(n)
+
+#! RUTA CON VARIABLES MIXTAS
+@app.route("/identificador/<int:id>/<string:username>")
+def userid(id,username):
+    return "El id es: {} y el nombre de usuario {}".format(id, username)
+
+#! RUTA DE CADENA DE TEXTO
+@app.route("/user/<string:user>")
+def user(user):
+    return "Hola"+ user
+
+#! RUTA NORMAL
+@app.route("/hola")
+def hola ():
+    return "Hola, un saludo"
+
+#! RUTA DEFAUL X DEFECTO
+@app.route("/default/") #* si no se especifica la ruta entonces arignara  yahoo por defecto de lo contrario la ruta seria la palabra cifrado
+@app.route("/default/<string:cifrado>") #*realmente no esta cifrado es solo una palabra de ejemplo
+def dft (cifrado = "Yahoo!!"):
+    return "El valor indicado es" + cifrado
+
+#! Comprueba si el módulo se está ejecutando como el programa principal.
 if __name__ == "__main__": #! Cuando se ejecuta un archivo Python, el valor de la variable __name__ se establece como "__main__".
     #*Si el módulo se está ejecutando como el programa principal, ejecuta la aplicación Flask.
     app.run(debug = True, port= 1991) #!La función app.run() inicia un servidor web local en el puerto 5000.
