7e464cf11f05476f09286b43fa6d9b9296b4cba8
carlos arnulfo cogua laverde <engineer.carloscogua@gmail.com>
2024-03-04 23:20:24 -0500

    Add a form with get method and putting a url for it funtionality

--- udemyapp/Uapp.py
+++ udemyapp/Uapp.py
@@ -14,12 +14,16 @@ def home ():
 @Uapp.route("/login")
 def login ():
     return "This is the login"
+
+@Uapp.route("/tu-url")
+def formulario ():
+    return render_template('form.html')
 #?-------------------END RUTES FUNTIONS ---------------------------------------------------------------
 #? -----------RUNNING APP-------------------------------------------------------------------------------
 #* Check if the module is running as principal program
 if __name__ == "__main__": # When executed an python file, the value of  variable __name__  is as  "__main__".
    with Uapp.app_context(): #  This line create a context of aplication for Uaap. This is necessary for that Run funtión working.
     #* If module is executing as principal program, it execute the app Uapp.
-    Uapp.run(debug = True) #the funtions execute for stard the local service
+    Uapp.run(debug = True) #the funtions execute for stard the local service, this metodh don't have to use in deploy only to use in development
 #* En resumen, el modo debug es una herramienta útil para principiantes que están aprendiendo a programar. Te ayuda a encontrar errores, entender tu código y probar cosas nuevas. Sin embargo, no es necesario para el producto final y puede ser un poco lento e informativo.
 #? ------------END RUUNING------------------------------------------------------------------------------
