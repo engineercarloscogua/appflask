3f504aa4c7af9cc36783c364a0e37b3cc7ea76e5
carlos arnulfo cogua laverde <engineer.carloscogua@gmail.com>
2024-03-23 22:33:25 -0500

    Improving the error messages for customizing languague

--- udemyapp/forms.py
+++ udemyapp/forms.py
@@ -17,14 +17,22 @@ from wtforms.validators import (
 #*  Definimos la clase LoginForm que hereda de FlaskForm para usar en html con jinja2
 class Loginform(FlaskForm): 
     # Definimos los campos del formulario junto con sus validadores
-    username = StringField('username', validators=[InputRequired(), Length(min= 4 ,max =16)])
-    password= PasswordField('password', validators=[InputRequired(), Length(min= 8 ,max =16)])
+    #this news changes help us for customizing the error messsages from our templates so we can have messages in spanish
+    username = StringField('username', validators=[InputRequired(message= 'Este campo requiere completarse'),
+    Length(min= 4 ,max =16, message= 'El Nombre de Usuario debe tener entre %(min)d y %(max)d Caracteres')])
+    
+    password= PasswordField('password', validators=[InputRequired(message= 'Este campo requiere completarse'),
+    Length(min= 8 ,max =16 , message= 'la contraseña debe etener entre  %(min)d y %(max)d Caracteres')])
+    
     remember = BooleanField('Recuerda me')
     
 #? ----------------------Register Form ----------------------------------------------
 class CreateUserForm(FlaskForm): 
     # Definimos los campos del formulario junto con sus validadores
-    email = StringField('Escribe el Correo Electronico', validators=[InputRequired(), Email(message="Este no es un correo electronico valido"), Length(max =60)])
-    username = StringField('Escribe el Nombre de Usuario', validators=[InputRequired(), Length(min= 4 ,max =16)]) # ahora puede cambiar el nombre de los label de l formulario desde aqui
-    password= PasswordField('Agrega una Contraseña', validators=[InputRequired(), Length(min= 8 ,max =16)])
+    email = StringField('Escribe el Correo Electronico', validators=[InputRequired(message= 'Este campo requiere completarse'), Email(message="Este no es un correo electronico valido"), 
+    Length(max =60 , message= 'El Nombre de Usuario debe tener  %(max)d Caracteres')])
+    username = StringField('Escribe el Nombre de Usuario', validators=[InputRequired(message= 'Este campo requiere completarse'),
+    Length(min= 4 ,max =16 , message= 'El Nombre de Usuario debe tener entre %(min)d y %(max)d Caracteres')]) # ahora puede cambiar el nombre de los label de l formulario desde aqui
+    password= PasswordField('Agrega una Contraseña', validators=[InputRequired(message= 'Este campo requiere completarse'), 
+    Length(min= 8 ,max =16 , message= 'la contraseña debe etener entre  %(min)d y %(max)d Caracteres')])
     
